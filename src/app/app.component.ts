5c293e91da937f6ac09d81d139c8d8fe5ed7f84b
Sudhanshu <bsudhanshu3@gmail.com>
2023-04-15 20:10:15 +0530

    autocomplete and highlight implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'insuredMine-assessment';
+  txt = '';
+
+  agents = [
+    'Gina Williams',
+    'Jake Williams',
+    'Jamie John',
+    'John Doe',
+    'Jeff Stewart',
+    'Paula M. Keith',
+  ];
 }
